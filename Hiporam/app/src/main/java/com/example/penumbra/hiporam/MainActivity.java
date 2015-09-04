2459ecc74d711622dc12b1c58501875ff0dc0726
Tolga Caner <tolga.caner@stu.bahcesehir.edu.tr>
2015-09-04 11:58:25 +0300

    actionbar v1

--- Hiporam/app/src/main/java/com/example/penumbra/hiporam/MainActivity.java
+++ Hiporam/app/src/main/java/com/example/penumbra/hiporam/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.penumbra.hiporam;
 
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
 
 
 public class MainActivity extends ActionBarActivity
@@ -14,31 +20,30 @@ protected void onCreate(Bundle savedInstanceState)
 {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    setUpActionBar();
 }
 
-
-@Override
-public boolean onCreateOptionsMenu(Menu menu)
+private void setUpActionBar()
 {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
+/*    ActionBar actionBar = getSupportActionBar();
+    actionBar.setDisplayShowHomeEnabled(true);
+    actionBar.setLogo(R.drawable.insta_logo);
+    actionBar.setDisplayUseLogoEnabled(true);
+    */
+    ActionBar actionBar = getSupportActionBar();
+    actionBar.setDisplayShowCustomEnabled(true);
+    actionBar.setDisplayShowTitleEnabled(false);
+    actionBar.setIcon(R.drawable.insta_logo);
+
+    actionBar.setHomeButtonEnabled(false); // disable the button
+    actionBar.setDisplayHomeAsUpEnabled(false); // remove the left caret
+    actionBar.setDisplayShowHomeEnabled(false); // remove the icon
+
+    LayoutInflater inflator = (LayoutInflater) this .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    View v = inflator.inflate(R.layout.action_bar_custom, null);
+
+    actionBar.setCustomView(v);
 }
 
-@Override
-public boolean onOptionsItemSelected(MenuItem item)
-{
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings)
-    {
-        return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-}
 }
